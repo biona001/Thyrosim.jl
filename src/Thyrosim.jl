667c61c73aca325d1c6d3bb80f15f4f0058a3b7b
Unknown <benchu99@hotmail.com>
2019-11-05 12:35:35 -0800

    remove fit.jl

--- src/Thyrosim.jl
+++ src/Thyrosim.jl
@@ -5,13 +5,12 @@ module Thyrosim
     export initialize, initialize_original_thyrosim
     export thyrosim, original_thyrosim
     export blakesley_data, jonklaas_data, schneider_data
-    export reset_p!, initialize!
+    export initialize!
 
 	using ExcelReaders, CSV
 
     include("thyrosim_odes.jl")
     include("utilities.jl")
-    include("fit.jl")
 
     # data directory, data not publically availble. 
 	datadir(parts...) = joinpath(@__DIR__, "..", "data", parts...)
